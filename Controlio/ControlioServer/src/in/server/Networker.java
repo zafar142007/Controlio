15fd0afa41a869ea7f8a30be8fefdce3b4567d0c
Zafar <zafar.ansari@walmartlabs.com>
2019-06-10 14:57:57 +0530

    added changes for test popup

--- Controlio/ControlioServer/src/in/server/Networker.java
+++ Controlio/ControlioServer/src/in/server/Networker.java
@@ -1,135 +1,145 @@
 package in.server;
+
 import in.control.Command;
 import in.control.CommandNotFoundException;
 import in.control.ControlScreen;
 import in.control.Dictionary;
-
+import in.server.util.ControlioConstants;
+import java.awt.Toolkit;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ClosedByInterruptException;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Networker implements Runnable {
-	//This thread will be started in ControlioServer and would be restarted if ControlioServer's
-	//setPort() is called
-	private ServerSocketChannel channel;
-	private int port=8079;
-	private ControlScreen remote=new ControlScreen();
-	private Dictionary dictionary=new Dictionary();
-	private	String host="localhost";
 
-	public int getPort() {
-		return port;
-	}
+  //This thread will be started in ControlioServer and would be restarted if ControlioServer's
+  //setPort() is called
+  private ServerSocketChannel channel;
+  private int port = 8079;
+  private ControlioServer server;
+  private ControlScreen remote = new ControlScreen();
+  private Dictionary dictionary = new Dictionary();
+  private String host = "localhost";
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+  public int getPort() {
+    return port;
+  }
 
-	public String getHost() {
-		return host;
-	}
+  public void setPort(int port) {
+    this.port = port;
+  }
 
-	public void setHost(String host) {
-		this.host = host;
-	}
+  public String getHost() {
+    return host;
+  }
 
-	public void setRemote(ControlScreen remote)
-	{
-		this.remote=remote;
+  public void setHost(String host) {
+    this.host = host;
+  }
 
-	}
-	public String sanitize(String string)
-	{
-		return string.toLowerCase().replace(' ', '_');
-	}
+  public void setRemote(ControlScreen remote) {
+    this.remote = remote;
 
-	public Command interpret(String command) throws CommandNotFoundException
-	{
-		Command[] commands=null;
+  }
 
-		commands=dictionary.getCommands(sanitize(command));
-		if(System.getProperty("os.name").toLowerCase().indexOf("win")>=0)
-		{
-			for(int i=0;i<commands.length;i++)
-				if(commands[i].getPlatform().toString().equals("win"))
-					return commands[i];
-		}
-		else
-		if(System.getProperty("os.name").toLowerCase().indexOf("nux")>=0)
-		{
-			for(int i=0;i<commands.length;i++)
-				if(commands[i].getPlatform().toString().equals("nix"))
-					return commands[i];
-		}
-		else
-		if(System.getProperty("os.name").toLowerCase().indexOf("mac")>=0)
-		{
-			for(int i=0;i<commands.length;i++)
-				if(commands[i].getPlatform().toString().equals("osx"))
-					return commands[i];
-		}
+  public String sanitize(String string) {
+    return string.toLowerCase().replace(' ', '_');
+  }
 
-		return null;
+  public Command interpret(String command) throws CommandNotFoundException {
+    Command[] commands = null;
+    if (isTest(command)) {
+      showTestStatus();
+    } else {
+      commands = dictionary.getCommands(sanitize(command));
+      if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+        for (int i = 0; i < commands.length; i++) {
+          if (commands[i].getPlatform().toString().equals("win")) {
+            return commands[i];
+          }
+        }
+      } else if (System.getProperty("os.name").toLowerCase().indexOf("nux") >= 0) {
+        for (int i = 0; i < commands.length; i++) {
+          if (commands[i].getPlatform().toString().equals("nix")) {
+            return commands[i];
+          }
+        }
+      } else if (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0) {
+        for (int i = 0; i < commands.length; i++) {
+          if (commands[i].getPlatform().toString().equals("osx")) {
+            return commands[i];
+          }
+        }
+      }
+    }
+    return null;
 
-	}
+  }
 
-	public void run() {
+  private boolean isTest(String command) {
+    return command.toLowerCase().equals(ControlioConstants.TEST_COMMAND);
+  }
 
-		// TODO Auto-generated method stub
-		try{
-			InetSocketAddress serverSocket;
-			int bytes=0;
-			channel=ServerSocketChannel.open();
-			serverSocket=new InetSocketAddress(InetAddress.getLocalHost(),port);
-			channel.bind(serverSocket);
-			
-			while(true)
-			{
-				System.out.println("Server: I am waiting for a connection");
-				SocketChannel ch=channel.accept();
-				System.out.println("Server: Accepted socket.");
-				bytes=0;
-				ByteBuffer buf = ByteBuffer.allocate(48);
-				while(bytes==0)
-				{
-					bytes= ch.read(buf);
-					if(bytes>0)
-					{
-						byte[] b=buf.array();
+  private void showTestStatus() {
+    JFrame alert = new JFrame();
+    alert.setSize(200, 70);
+    alert.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    alert.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - 200,
+        Toolkit.getDefaultToolkit().getScreenSize().height / 2 - 70);
+    alert.setTitle("Setup");
+    try {
+      alert.add(new JLabel("You are all set!\nYou can now close this popup."));
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    alert.setVisible(true);
+  }
 
-						String command=new String(b);
-						buf.clear();
-						System.out.println("Client: "+command);
-						try{
-							remote.execute(interpret(command));
-						}
-						catch(CommandNotFoundException e)
-						{
-							System.out.println("Server: Invalid command: "+e);
-						}
-						System.out.println("Network Thread: Closing channel");
-						
-						//channel.socket().close();
-						//channel.close();
-						//ch.socket().close();
-						//ch.close();
-						
-						break;
-					}
-				}								
-			}
-		}
-		catch(ClosedByInterruptException ex)
-		{
-			//ex.printStackTrace();
-			System.out.println("Network Thread: I have been interrupted");
-		}catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+
+
+  public void run() {
+
+    try {
+      InetSocketAddress serverSocket;
+      int bytes = 0;
+      channel = ServerSocketChannel.open();
+      serverSocket = new InetSocketAddress(InetAddress.getLocalHost(), port);
+      channel.bind(serverSocket);
+
+      while (true) {
+        System.out.println("Server: I am waiting for a connection");
+        SocketChannel ch = channel.accept();
+        System.out.println("Server: Accepted socket.");
+        bytes = 0;
+        ByteBuffer buf = ByteBuffer.allocate(48);
+        while (bytes == 0) {
+          bytes = ch.read(buf);
+          if (bytes > 0) {
+            byte[] b = buf.array();
+
+            String command = new String(b);
+            buf.clear();
+            System.out.println("Client: " + command);
+            try {
+              remote.execute(interpret(command));
+            } catch (CommandNotFoundException e) {
+              System.out.println("Server: Invalid command: " + e);
+            }
+            break;
+          }
+        }
+      }
+    } catch (ClosedByInterruptException ex) {
+      //ex.printStackTrace();
+      System.out.println("Network Thread: I have been interrupted");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
 }
